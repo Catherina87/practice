7fdbc64f1817192441c9adb730175048f940c80a
Catherina <catherine.pchelina@gmail.com>
2020-04-12 15:46:49 -0700

    reverse words in place

---
+++ ruby/reverse_words_in_place.rb
@@ -0,0 +1,33 @@
+def reverse_phrase(phrase)
+  return phrase if phrase.nil? || phrase.length == 0 || phrase == ""
+
+  # reverse all chars in phrase
+  reversed_chars = reverse_characters(phrase, 0, phrase.length - 1)
+
+  current_word_start_index = 0
+
+  (0..phrase.length).each do |i|
+    if phrase[i] == " " || i == phrase.length
+      reverse_characters(phrase, current_word_start_index, i - 1)
+      current_word_start_index = i + 1
+    end
+  end
+
+  return phrase
+end
+
+def reverse_characters(phrase, first_index, last_index)
+  while first_index < last_index
+    temp = phrase[first_index]
+    phrase[first_index] = phrase[last_index]
+    phrase[last_index] = temp
+
+    first_index += 1
+    last_index -= 1
+  end
+
+  return phrase
+end
+
+p reverse_phrase("  I can do this!     ")
+p reverse_phrase("apple cake")
